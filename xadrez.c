4017719418d7249d7267d77072867a89bb70a700
Dev_Laiannesouza <laiannecris47@gmail.com>
2025-05-23 19:06:45 +0000

    Finalizado

--- xadrez.c
+++ xadrez.c
@@ -8,6 +8,7 @@ int main() {
     printf ("Movimento da Torre:\n ");
     for ( int i = 0; i < casasTorre; i++){
         printf ("Direita\n");
+       
     }
 
     //Movimento do Bispo : 5 casas na diagonal (cima, direita) usando while
@@ -16,23 +17,18 @@ int main() {
     printf("\nMovimento do Bispo:\n");
     while (i < casasBispo){
         printf("Cima Direita\n");
+        i++;
     }
 
-    // 
+    // Movimento da Rainha : 8 casas a Esquerda (usando do-while)
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    int casasRainha = 8;
+    int j = 0;
+    printf ("\nMovimento da Rainha: \n");
+    do {
+        printf ("Esquerda \n");
+        j++;
+    }while (j < casasRainha);
 
     return 0;
 }
