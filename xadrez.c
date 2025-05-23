a0304fa8e482519579600024848eba0f585610a7
Dev_Laiannesouza <laiannecris47@gmail.com>
2025-05-23 18:30:28 +0000

    moviemento Torre

--- xadrez.c
+++ xadrez.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // Movimento da Torre : 5 casas para direita
+    int casasTorre = 5;
+    printf ("Movimento da Torre:\n ");
+    for ( int i = 0; i < casasTorre; i++){
+        printf ("Direita\n");
+    }
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
